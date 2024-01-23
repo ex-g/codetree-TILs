deb1cce73d27bb9cefb162769e41bf53ff1140f6
김예지 <100893821+ex-g@users.noreply.github.com>
2024-01-23 22:23:39 +0900

    Title: 문자 제거하기, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240123/문자 제거하기/del-char.py
+++ 240123/문자 제거하기/del-char.py
@@ -4,10 +4,10 @@ leng = len(str)
 while leng > 1:
     erase = int(input())
 
-    if erase > len(str):
+    if erase >= len(str):
         str.pop()
     else:
         str.pop(erase)
     leng -= 1
-    
+
     print("".join(str))
