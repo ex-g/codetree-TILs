4bfda6625fed8b3a2b46da42117c8cb7208d0f90
김예지 <100893821+ex-g@users.noreply.github.com>
2024-01-23 22:22:30 +0900

    Title: 문자 제거하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240123/문자 제거하기/del-char.py
+++ 240123/문자 제거하기/del-char.py
@@ -1,9 +1,13 @@
 str = list(input())
+leng = len(str)
 
-while len(str) > 1:
+while leng > 1:
     erase = int(input())
+
     if erase > len(str):
         str.pop()
     else:
         str.pop(erase)
+    leng -= 1
+    
     print("".join(str))
