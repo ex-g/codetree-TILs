e89a9243d2d1fd9a8544aadcb5a1e470ece30c54
김예지 <100893821+ex-g@users.noreply.github.com>
2024-01-23 23:02:09 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240123/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240123/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -3,16 +3,19 @@ arr = list(map(int, input().split()))
 arr.sort(reverse = True)
 
 ans = -1
-for i in range(N-1):
-    if i == 0 and arr[i] != arr[i+1]:
-        ans = arr[i]
-        break
-    elif i == N-1 and arr[i] != arr[i-1]:
-        ans = arr[i]
-        break
-    else:
-        if arr[i] != arr[i-1] and arr[i] != arr[i + 1]:
+if N == 1:
+    ans = arr[0]
+else:
+    for i in range(N-1):
+        if i == 0 and arr[i] != arr[i+1]:
             ans = arr[i]
             break
+        elif i == N-1 and arr[i] != arr[i-1]:
+            ans = arr[i]
+            break
+        else:
+            if arr[i] != arr[i-1] and arr[i] != arr[i + 1]:
+                ans = arr[i]
+                break
 
 print(ans)
