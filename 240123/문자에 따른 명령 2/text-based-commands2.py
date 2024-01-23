b4326891e9f4dfef2eebb613d1c6f97e43e04be9
김예지 <100893821+ex-g@users.noreply.github.com>
2024-01-23 11:19:43 +0900

    Title: 문자에 따른 명령 2, Time: 160ms, Memory: 26MB, Status: Passed - CodeTree

--- 240123/문자에 따른 명령 2/text-based-commands2.py
+++ 240123/문자에 따른 명령 2/text-based-commands2.py
@@ -1,15 +1,15 @@
 dir_lst = list(input())
 x, y = 0, 0
+dir_num = 0
 dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
 
-dir = 0
-dir_dict = {"L": (dir + 3) % 4, "R": (dir + 1) % 4}
 
 for elem in dir_lst:
+    dir_dict = {"L": (dir_num + 3) % 4, "R": (dir_num + 1) % 4}
     if elem == "F":
-        x = x + dx[dir]
-        y = y + dy[dir]
+        x = x + dx[dir_num]
+        y = y + dy[dir_num]
     else:
-        dir = dir_dict[elem]
+        dir_num = dir_dict[elem]
 
 print(x, y)
