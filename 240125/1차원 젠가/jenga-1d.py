abb385df87c688e8253e9605070ce4cf4814e0c9
김예지 <100893821+ex-g@users.noreply.github.com>
2024-01-25 12:38:59 +0900

    Title: 1차원 젠가, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240125/1차원 젠가/jenga-1d.py
+++ 240125/1차원 젠가/jenga-1d.py
@@ -1,31 +1,21 @@
 n = int(input())
-arr = [int(input()) for _ in range(n)]
-s1, e1 = map(int, input().split())
-s2, e2 = map(int, input().split())
+numbers = [int(input()) for _ in range(n)]
+end_of_array = n
+
+def cut_array(s, e):
+    global numbers, end_of_array
+
+    cut_len = e - s + 1
+    for i in range(e + 1, end_of_array):
+        numbers[i - cut_len] = numbers[i]
+    end_of_array -= cut_len
+    numbers = numbers[:end_of_array]
 
 
-def play(arr, start, end):
-    BLANK = 0
-    temp = [0] * n
-    end_of_array, end_of_temp = 0, 0
+for i in range(2):
+    s, e = map(int, input().split())
+    cut_array(s-1, e-1)
 
-    for i in range(start-1, end):
-        arr[i] = BLANK
-
-    for i in range(len(arr)):
-        if arr[i] != BLANK:
-            temp[end_of_temp] = arr[i]
-            end_of_temp += 1
-        
-    for i in range(end_of_temp):
-        arr[i] = temp[i]
-    end_of_array = end_of_temp
-
-    return arr[:end_of_temp]
-
-arr = play(arr, s1, e1)
-arr = play(arr, s2, e2)
-
-print(len(arr))
-for i in range(len(arr)):
-    print(arr[i])
+print(len(numbers))
+for elem in numbers:
+    print(elem)
