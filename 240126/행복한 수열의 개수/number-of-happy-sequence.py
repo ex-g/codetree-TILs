857f43d5cd798f1cdcd5b9200288c151fc23d320
김예지 <100893821+ex-g@users.noreply.github.com>
2024-01-26 14:27:48 +0900

    Title: 행복한 수열의 개수, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240126/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240126/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,25 +1,35 @@
 n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
 result = 0
-arr = []
-for _ in range(n):
-    lst = list(map(int, input().split()))
-    cnt = 0
-    for i in range(n-1):
-        if lst[i] == lst[i+1]:
+
+# 가로부터 한 줄씩 꺼내오기
+# 첫 숫자랑 둘째 숫자랑 같으면 cnt+1, 아니면 cnt 1로 초기화
+# cnt가 m이상이면 result += 1
+
+for i in range(n):
+    lst = arr[i]
+    cnt = 1
+    for j in range(n-1):
+        if arr[i][j] == arr[i][j+1]:
             cnt += 1
-    if cnt >= m -1:
+        else:
+            cnt = 1
+    if cnt >= m:
         result += 1
-    arr.append(lst)
+
+# 세로도 똑같이 진행
 
 for i in range(n):
     lst = []
     for j in range(n):
         lst.append(arr[j][i])
-    cnt = 0
-    for i in range(n-1):
-        if lst[i] == lst[i+1]:
+    cnt = 1
+    for j in range(n-1):
+        if arr[i][j] == arr[i][j+1]:
             cnt += 1
-    if cnt >= m - 1:
+        else:
+            cnt = 1
+    if cnt >= m:
         result += 1
 
 print(result)
