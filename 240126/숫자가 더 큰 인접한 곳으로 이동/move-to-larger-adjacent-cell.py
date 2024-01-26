7e1cf4bd0423315f19013fbd8b7769d383cb3444
김예지 <100893821+ex-g@users.noreply.github.com>
2024-01-26 14:01:47 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 145ms, Memory: 25MB, Status: Passed - CodeTree

--- 240126/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 240126/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -1,15 +1,27 @@
 n, r, c= map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
 r, c = r-1, c-1
-print(grid[r][c], end= " ")
+grid = [list(map(int, input().split())) for _ in range(n)]
 dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
 
 def in_range(r, c):
     return 0 <= r < n and 0 <= c < n
 
+def simulate():
+    global r, c
+    max_num = grid[r][c]
+    judge = False
 
-for dx, dy in zip(dxs, dys):
-    nr, nc = r + dx, c + dy
-    if in_range(nr, nc) and grid[r][c] < grid[nr][nc]:
-        print(grid[nr][nc], end=" ")
-        r, c = nr, nc
+    for dx, dy in zip(dxs, dys):
+        nr, nc = r + dx, c + dy
+
+        if in_range(nr, nc) and max_num < grid[nr][nc]:
+            max_num = grid[nr][nc]
+            print(max_num, end = " ")
+            r, c = nr, nc
+            judge = True
+            return judge
+
+print(grid[r][c], end= " ")
+
+for _ in range(n * n):
+    simulate()
