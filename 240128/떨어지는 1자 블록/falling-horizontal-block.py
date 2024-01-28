77e67ef778259e184d7b41681bbc741d26cd48cd
김예지 <100893821+ex-g@users.noreply.github.com>
2024-01-28 16:47:02 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240128/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,18 @@
+n, m, k = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+def check():
+    for row in range(1, n):
+        for col in range(k - 1, k + m - 2):
+            if grid[row][col] == 1:
+                return row
+
+final_row = check()
+
+for col in range(k - 1, k + m - 1):
+    grid[final_row - 1][col] = 1
+
+for row in range(n):
+    for col in range(n):
+        print(grid[row][col], end=" ")
+    print()
