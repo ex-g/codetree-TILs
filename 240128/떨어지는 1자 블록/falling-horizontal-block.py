9f82bd9a1e5b153d064796c2efca8b0f1097305a
김예지 <100893821+ex-g@users.noreply.github.com>
2024-01-28 18:02:31 +0900

    Title: 떨어지는 1자 블록, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 240128/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240128/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,27 +1,18 @@
 n, m, k = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-def zero_check():
-    judge = True
-    for i in range(n):
-        if not all(num == 0 for num in grid[i]):
-            judge = False
-    return judge
+point_row = 101
+for row in range(0, n):
+    for col in range(k - 1, k - 1 + m):
+        if grid[row][col] == 1 and point_row > row:
+            point_row = row
+            break
+            
+if point_row == 101:
+    point_row = 0
 
-def check():
-    for row in range(1, n):
-        for col in range(k - 1, k + m - 2):
-            if grid[row][col] == 1:
-                return row
-if n == 1:
-    final_row = 1
-elif zero_check():
-    final_row = 0
-else:
-    final_row = check()
-
-for col in range(k - 1, k + m - 1):
-    grid[final_row - 1][col] = 1
+for col in range(k-1, k-1+m):
+    grid[point_row - 1][col] = 1
 
 for row in range(n):
     for col in range(n):
