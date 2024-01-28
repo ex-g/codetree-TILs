342156812f25fc34bd049e00ef0d23192a8a38ca
김예지 <100893821+ex-g@users.noreply.github.com>
2024-01-28 15:31:11 +0900

    Title: 두 사람, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240128/두 사람/two-person.py
+++ 240128/두 사람/two-person.py
@@ -1,5 +1,5 @@
 age1, gender1 = input().split()
 age2, gender2 = input().split()
 
-if (int(age1) >= 19 and gender1 == "M") or (int(age2) >=19 and gender2 == "W"):
+if (int(age1) >= 19 and gender1 == "M") or (int(age2) >=19 and gender2 == "M"):
     print(1)
