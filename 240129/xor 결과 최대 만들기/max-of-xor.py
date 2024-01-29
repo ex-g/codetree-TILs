43438b5af952a068a039be9b54ea42c677858ef6
김예지 <100893821+ex-g@users.noreply.github.com>
2024-01-29 12:52:06 +0900

    Title: xor 결과 최대 만들기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240129/xor 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,30 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+chosen = []
+max_result = 0
+
+def xor(chosen):
+    result = 0
+    for elem in chosen:
+        result ^= elem
+    return result
+
+def choose(cnt, last_num):
+    global max_result
+
+    if cnt == m:
+        max_result = max(max_result, xor(chosen))
+        return
+    
+    for i in range(last_num + 1, n + 1):
+        chosen.append(i)
+        choose(cnt + 1, i)
+        chosen.pop()
+
+    
+for i in range(n):
+    chosen.append(arr[i])
+    choose(1, arr[i])
+    chosen.pop()
+
+print(max_result)
