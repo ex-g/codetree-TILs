f3c057898610dd4e1ae00608dcc23b70228656f1
김예지 <100893821+ex-g@users.noreply.github.com>
2024-01-31 11:37:36 +0900

    Title: 마을 구분하기, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240131/마을 구분하기/seperate-village.py
+++ 240131/마을 구분하기/seperate-village.py
@@ -1,5 +1,6 @@
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
 cnt_lst = []
 cnt = 0
 vil = 0
@@ -7,31 +8,35 @@ vil = 0
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
+def can_go(x, y):
+    if not in_range(x, y) or visited[x][y] == True or graph[x][y] != 1:
+        return False
+    return True
+
 
 def dfs(x, y):
     global cnt
+
+    cnt += 1
+    visited[x][y] = True
 
     dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
 
     for dx, dy in zip(dxs, dys):
         nx, ny = x + dx, y + dy
 
-        if in_range(nx, ny) and graph[nx][ny] == 1:
-            cnt += 1
-            graph[nx][ny] = 0
+        if can_go(nx, ny):
             dfs(nx, ny)
     
-    if cnt != 0:
-        cnt_lst.append(cnt)
-    cnt = 0
-
+    
 for row in range(n):
     for col in range(n):
-        if graph[row][col] == 1:
-            graph[row][col] = 0
-            cnt += 1
+        if graph[row][col] == 1 and visited[row][col] == False:
             vil += 1
             dfs(row, col)
+        if cnt != 0:
+            cnt_lst.append(cnt)
+            cnt = 0
 
 
 print(vil)
