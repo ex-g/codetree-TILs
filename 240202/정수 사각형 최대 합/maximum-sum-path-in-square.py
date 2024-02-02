2b2d395b54cc8366258896a393a395029bd2d4eb
김예지 <100893821+ex-g@users.noreply.github.com>
2024-02-02 16:55:14 +0900

    Title: 정수 사각형 최대 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240202/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -0,0 +1,24 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
+
+dp[0][0] = grid[0][0]
+
+def initialize():
+    for i in range(1, n):
+        dp[i][0] = dp[i-1][0] + grid[i][0]
+
+    for i in range(1, n):
+        dp[0][i] = dp[0][i-1] + grid[0][i]
+
+initialize()
+
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = max(dp[i][j-1], dp[i-1][j]) + grid[i][j]
+
+ans = 0
+for i in range(n):
+    ans = max(ans, dp[n-1][j])
+
+print(ans)
