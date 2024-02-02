eb055160b1b337a1227d85120aeb9fe39f7f2f66
김예지 <100893821+ex-g@users.noreply.github.com>
2024-02-02 18:43:18 +0900

    Title: 최대 증가 부분 수열, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240202/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 240202/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -1,27 +1,17 @@
 import sys
-INT_MIN = -sys.maxsize
 
 n = int(input())
 arr = list(map(int, input().split()))
-dp = [0] * n
-
-def init():
-    for i in range(n):
-        dp[i] = INT_MIN
-
-    dp[0] = 1
-
-init()
+dp = [1] * n
 
 for i in range(1, n):
-    for j in range(0, i):
-        if dp[j] == INT_MIN:
-            continue
+    for j in range(i):
         
         if arr[j] < arr[i]:
             dp[i] = max(dp[i], dp[j] + 1)
 
-ans = -sys.maxsize
+
+ans = 0
 for i in range(n):
     ans = max(ans, dp[i])
 
