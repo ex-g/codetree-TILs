21fd0e61ebde78ec152782f3bd28d845bb783f22
김예지 <100893821+ex-g@users.noreply.github.com>
2024-02-02 11:40:18 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240202/피보나치 수/fibonacci-number.py
+++ 240202/피보나치 수/fibonacci-number.py
@@ -1,9 +1,13 @@
+memo = [-1] * 45
+
+def fibbo(n):
+    if memo[n] != -1:
+        return memo[n]
+    if n <= 2:
+        memo[n] = 1
+    else:
+        memo[n] = fibbo(n-1) + fibbo(n-2)
+    return memo[n]
+
 n = int(input())
-fibo = [0] * 45
-fibo[1] = 1
-fibo[2] = 1
-
-for i in range(3, n + 1):
-    fibo[i] = fibo[i-1] + fibo[i-2]
-
-print(fibo[n])
+print(fibbo(n))
