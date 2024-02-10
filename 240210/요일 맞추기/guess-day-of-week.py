44e0cfbbf2fe81eb63960692c6b5129a70991d63
김예지 <100893821+ex-g@users.noreply.github.com>
2024-02-10 20:31:39 +0900

    Title: 요일 맞추기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240210/요일 맞추기/guess-day-of-week.py
+++ 240210/요일 맞추기/guess-day-of-week.py
@@ -1,6 +1,6 @@
 m1, d1, m2, d2 = map(int, input().split())
 
-dates = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+dates = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 days_of_months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 start, end = 0, 0
@@ -13,4 +13,7 @@ for i in range(m2):
     end += days_of_months[i]
 end += d2
 
-print(dates[end % 7 - 4])
+if start <= end:
+    print(dates[start % 7 - end % 7])
+else:
+    print(dates[end % 7 - start % 7])
