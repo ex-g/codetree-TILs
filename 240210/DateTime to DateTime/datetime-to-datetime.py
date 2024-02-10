0170984ff319f3027dff43d430b6c20011ed3de1
김예지 <100893821+ex-g@users.noreply.github.com>
2024-02-10 20:20:20 +0900

    Title: DateTime to DateTime, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240210/DateTime to DateTime/datetime-to-datetime.py
+++ 240210/DateTime to DateTime/datetime-to-datetime.py
@@ -3,7 +3,9 @@ a, b, c = map(int, input().split())
 start = 11 * 60 + 11
 end = b * 60 + c
 
-if end < start:
-    print(-1)
-else:
+if a > 11:
     print(end - start + (a - 11) * 60 * 24)
+elif a == 11 and end < start:
+    print(-1)
+elif a == 11 and end >= start:
+    print(end - start)
