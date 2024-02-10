413abea8c31f3eea431ff997f92f2d49de265c00
김예지 <100893821+ex-g@users.noreply.github.com>
2024-02-10 21:00:44 +0900

    Title: 그 요일은, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240210/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,30 @@
+m1, d1, m2, d2 = map(int, input().split())
+days_of_months = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+start, end = 0, 0
+
+for i in range(m1):
+    start += days_of_months[i]
+start += d1
+
+for i in range(m2):
+    end += days_of_months[i]
+end += d2
+
+date = input()
+dates = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+dates_dict = {}
+
+start_point = start % 7
+
+for i in dates:
+    dates_dict[i] = start_point
+    start_point += 1
+
+point = dates_dict[date]
+
+result = 0
+for i in range(start, end + 1):
+    if i % 7 == point:
+        result += 1
+
+print(result)
