cf217cba2e673531ed9a5e5d85c89dd9f6106243
김예지 <100893821+ex-g@users.noreply.github.com>
2024-05-24 13:36:34 +0900

    Title: 되돌아오기, Time: 137ms, Memory: 72MB, Status: Passed - CodeTree

--- 240524/되돌아오기/come-back.py
+++ 240524/되돌아오기/come-back.py
@@ -1,21 +1,23 @@
 n = int(input())
+x, y = 0, 0
 dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+ans = -1
+time = 0
 mapper = {
     "E" : 0,
     "S" : 1,
     "W" : 2,
     "N" : 3
 }
-time = 0
-x, y = 0, 0
 
 def move(d, dist):
-    global x, y, time 
+    global x, y, ans, time 
 
     for _ in range(dist):
         x, y = x + dxs[d], y + dys[d]
         time += 1
         if x == 0 and y == 0:
+            ans = time
             return True
     return False
 
@@ -24,7 +26,9 @@ for _ in range(n):
     d, dist = tuple(input().split())
     d, dist = mapper[d], int(dist)
 
-    if move(d, dist):
+    done = move(d, dist)
+
+    if done:
         break
 
-print(time)
+print(ans)
