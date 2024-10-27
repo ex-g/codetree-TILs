6386ea112d341a9975a0e3bad0f3c691442492d5
김예지 <100893821+ex-g@users.noreply.github.com>
2024-10-27 19:41:03 +0900

    Title: 특정 구간의 원소 평균값, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 241027/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 241027/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -7,8 +7,14 @@ for i in range(N):
         average = 0
         for k in range(i, j + 1):
             average += arr[k]
-        average //= j - i + 1
-        if any(arr) == average:
-            cnt += 1
+        average /= (j - i + 1)
+        # print(i, j)
+        # print("average : ", average)
+        for l in range(i, j + 1):
+            # print(float(arr[l]))
+            if float(arr[l]) == average:
+                # print("arr[l] : ", arr[l], "\n")
+                cnt += 1
+                break
 
 print(cnt)
