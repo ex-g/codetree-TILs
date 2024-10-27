a5de27c6ea3b979d159c8db7cba2de6b4403897d
김예지 <100893821+ex-g@users.noreply.github.com>
2024-10-27 18:46:01 +0900

    Title: 이상한 진수 2, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 241027/이상한 진수 2/awkward-digits-2.py
+++ 241027/이상한 진수 2/awkward-digits-2.py
@@ -1,13 +1,13 @@
 a = list(input())
 max_val = 0
 
-if all(a) == "1":
-    a[-1] = "0"
-else:
+if "0" in a:
     for i in range(len(a)):
         if a[i] == "0":
             a[i] = "1"
-            break
+            break   
+else:
+    a[-1] = "0"
 
 answer = "".join(a)
 print(int(answer, 2))
