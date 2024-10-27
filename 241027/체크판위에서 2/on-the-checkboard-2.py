4138caec292291a6b6f9b8db79543f99d4a5e874
김예지 <100893821+ex-g@users.noreply.github.com>
2024-10-27 18:24:40 +0900

    Title: 체크판위에서 2, Time: 165ms, Memory: 28MB, Status: Passed - CodeTree

--- 241027/체크판위에서 2/on-the-checkboard-2.py
+++ 241027/체크판위에서 2/on-the-checkboard-2.py
@@ -6,7 +6,7 @@ for i in range(1, R-2):
     for j in range(1, C-2):
         for k in range(i+1, R-1):
             for l in range(j+1, C-1):
-                if grid[i][j] != grid[0][0] and grid[k][l] != grid[R-1][C-1]:
+                if grid[i][j] != grid[0][0] and grid[k][l] != grid[R-1][C-1] and grid[0][0] != grid[R-1][C-1]:
                     cnt += 1
 
 print(cnt)
