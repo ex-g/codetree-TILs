088453be10acf4a03759901a3a11b0cbf8057555
김예지 <100893821+ex-g@users.noreply.github.com>
2024-12-08 15:27:03 +0900

    Title: 3N + 1 수열, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241208/3N + 1 수열/3n-plus-1-sequence.py
+++ 241208/3N + 1 수열/3n-plus-1-sequence.py
@@ -3,14 +3,16 @@ cnt = 0
 
 while True:
 
+    if N == 1:
+        break
+
     if N % 2 == 0:
         N //= 2
     else:
         N = N * 3 + 1
     cnt += 1
 
-    if N == 1:
-        break
+
 
 
 print(cnt)
