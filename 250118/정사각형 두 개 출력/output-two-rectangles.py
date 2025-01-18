830112b48b6600ab587557c4e2fbd77e620b905c
김예지 <100893821+ex-g@users.noreply.github.com>
2025-01-18 09:04:15 +0900

    Title: 정사각형 두 개 출력, Time: 72ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250118/정사각형 두 개 출력/output-two-rectangles.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        print("*", ="")
+    print()
+
+for i in range(n):
+    for j in range(n):
+        print("*", ="")
+    print()
